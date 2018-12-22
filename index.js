7622d93574c4454b6a98638ea673f23b28d034f2
Hugo Burbelo <hugoburbelo@gmail.com>
2018-12-22 17:20:03 -0500

    Add basic express server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const server = app.listen(4000, function(){
+  console.log('listening to requests on port 4000')
+})
+
+app.use(express.static('public'));
